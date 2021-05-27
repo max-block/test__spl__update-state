dd1e2eef604e19b920088cb4d7b5ff022818cfd0
Max Block <maxblock@pm.me>
2021-05-27 20:04:24 +0300

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,25 @@
 use borsh::{BorshDeserialize, BorshSerialize};
-use solana_program::{account_info::{AccountInfo, next_account_info}, entrypoint, entrypoint::ProgramResult, msg, nonce::state, pubkey::Pubkey};
+use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    entrypoint,
+    entrypoint::ProgramResult,
+    msg,
+    pubkey::Pubkey,
+};
 
 entrypoint!(process_instruction);
 
-#[derive(BorshSerialize, BorshDeserialize,  Debug)]
+#[derive(BorshSerialize, BorshDeserialize, Debug)]
 pub struct State {
     pub counter: u16,
     pub f1: i32,
-    pub f2: String,
+    pub f2: u64,
 }
 
 #[derive(BorshSerialize, BorshDeserialize, Debug)]
 pub struct InstructionData {
     pub f1: i32,
-    pub f2: String,
+    pub f2: u64,
 }
 
 pub fn process_instruction(_program_id: &Pubkey, accounts: &[AccountInfo], instruction_data: &[u8]) -> ProgramResult {
@@ -34,19 +40,8 @@ pub fn process_instruction(_program_id: &Pubkey, accounts: &[AccountInfo], instr
     state.serialize(&mut &mut account.data.borrow_mut()[..]);
     msg!("new state: {:?}", state);
 
-    
+    msg!("data: {:?}", &account.data);
 
-    // let mut zzz = new_state.try_to_vec().unwrap();
-    
-    // msg!("zzz: {:?}", zzz.as_slice());
-
-    
-    // let  data: &mut[u8] = &mut[];
-    // *account.data.borrow_mut() =  new_state.try_to_vec().unwrap().as_mut_slice();
-    
-    
-
-    
     msg!("update-state finish");
     Ok(())
 }
@@ -57,9 +52,14 @@ mod test {
 
     #[test]
     fn test_serialization() {
-        let a1 = InstructionData { f1: 777, f2: String::from("foo") }.try_to_vec().unwrap();
+        let a1 = InstructionData { f1: 777, f2: 999 }.try_to_vec().unwrap();
         println!("z0: {:?}", a1);
-        let ix = InstructionData::try_from_slice(&[9, 3, 0, 0, 3, 0, 0, 0, 102, 111, 111]).unwrap();
+        let ix = InstructionData::try_from_slice(&[9, 3, 0, 0, 231, 3, 0, 0, 0, 0, 0, 0]).unwrap();
         println!("z1: {:?}", ix);
+
+        let s0 = State { counter: 1, f1: 17, f2: 108 }.try_to_vec().unwrap();
+        println!("s0: {:?}", s0);
+        let s1 = State::try_from_slice(&[1, 0, 17, 0, 0, 0, 108, 0, 0, 0, 0, 0, 0, 0]);
+        println!("s1: {:?}", s1);
     }
 }
--- tests/lib.rs
+++ tests/lib.rs
@@ -1,8 +1,13 @@
-use borsh::{BorshSerialize};
+use borsh::{BorshDeserialize, BorshSerialize};
 use solana_program::instruction::{AccountMeta, Instruction};
 use solana_program_test::*;
-use solana_sdk::{account::Account, pubkey::Pubkey, signature::Signer, transaction::Transaction};
-use update_state::{process_instruction, State, InstructionData};
+use solana_sdk::{
+    account::{Account, ReadableAccount},
+    pubkey::Pubkey,
+    signature::Signer,
+    transaction::Transaction,
+};
+use update_state::{process_instruction, InstructionData, State};
 
 #[tokio::test]
 async fn test_program() {
@@ -10,25 +15,29 @@ async fn test_program() {
     let state_pubkey = Pubkey::new_unique();
 
     let mut program_test = ProgramTest::new("update_state", program_id, processor!(process_instruction));
-    let data = State{counter: 0, f1: 0, f2: String::from("")}.try_to_vec().unwrap();
+    let data = State { counter: 0, f1: 0, f2: 0 }.try_to_vec().unwrap();
     program_test.add_account(state_pubkey, Account { lamports: 77777, data, owner: program_id, ..Account::default() });
-    
 
     let (mut banks_client, payer, recent_blockhash) = program_test.start().await;
 
-    
+    // Call program 10 times
+    for i in 0..10 {
+        let init_data = InstructionData { f1: 18 + i, f2: 109 + i as u64 }.try_to_vec().unwrap();
+        let mut transaction = Transaction::new_with_payer(
+            &[Instruction::new_with_bytes(program_id, &init_data, vec![AccountMeta::new(state_pubkey, false)])],
+            Some(&payer.pubkey()),
+        );
+        transaction.sign(&[&payer], recent_blockhash);
+        banks_client.process_transaction(transaction).await.unwrap();
+    }
 
+    // Check counter in state account
+    let state_account = banks_client.get_account(state_pubkey).await.unwrap().unwrap();
+    println!("state_account: {:?}", state_account);
+    let data = state_account.data().as_slice();
+    println!("z1: {:?}", data);
+    let new_state = State::try_from_slice(data).unwrap();
+    println!("new state: {:?}", new_state);
 
-    // Call program first time
-    let init_data = InstructionData { f1: 1, f2: String::from("foo") }.try_to_vec().unwrap();
-    let mut transaction = Transaction::new_with_payer(
-        &[Instruction::new_with_bytes(program_id, &init_data, vec![AccountMeta::new(state_pubkey, false)])],
-        Some(&payer.pubkey()),
-    );
-    transaction.sign(&[&payer], recent_blockhash);
-    banks_client.process_transaction(transaction).await.unwrap();
-
-    // // Call program
-    // let state_account = banks_client.get_account(state_pubkey).await;
-    // println!("state_account: {:?}", state_account);
+    assert_eq!(new_state.counter, 10);
 }
