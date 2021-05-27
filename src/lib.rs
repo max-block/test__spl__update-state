49b2a185568d6ef7fe8c598e8771058488a747b1
Max Block <maxblock@pm.me>
2021-05-27 18:51:39 +0300

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,13 @@
 use borsh::{BorshDeserialize, BorshSerialize};
-use solana_program::{
-    account_info::{next_account_info, AccountInfo},
-    entrypoint,
-    entrypoint::ProgramResult,
-    msg,
-    pubkey::Pubkey,
-};
+use solana_program::{account_info::{AccountInfo, next_account_info}, entrypoint, entrypoint::ProgramResult, msg, nonce::state, pubkey::Pubkey};
 
 entrypoint!(process_instruction);
 
-#[derive(BorshSerialize, BorshDeserialize, Debug)]
+#[derive(BorshSerialize, BorshDeserialize,  Debug)]
 pub struct State {
-    counter: u16,
-    f1: i32,
-    f2: String,
+    pub counter: u16,
+    pub f1: i32,
+    pub f2: String,
 }
 
 #[derive(BorshSerialize, BorshDeserialize, Debug)]
@@ -25,21 +19,40 @@ pub struct InstructionData {
 pub fn process_instruction(_program_id: &Pubkey, accounts: &[AccountInfo], instruction_data: &[u8]) -> ProgramResult {
     msg!("update-state start: {:?}", instruction_data);
     let account_info_iter = &mut accounts.iter();
-    let state_account = next_account_info(account_info_iter)?;
+    let account = next_account_info(account_info_iter)?;
 
-    msg!("z0");
+    msg!("account: {:?}", account);
     let instruction = InstructionData::try_from_slice(instruction_data)?;
     msg!("instruction: {:?}", instruction);
 
-    msg!("state_account: {:?}", state_account);
+    let mut state = State::try_from_slice(&account.data.borrow())?;
+    msg!("prev state: {:?}", state);
+
+    state.counter += 1;
+    state.f1 = instruction.f1;
+    state.f2 = instruction.f2;
+    state.serialize(&mut &mut account.data.borrow_mut()[..]);
+    msg!("new state: {:?}", state);
+
+    
+
+    // let mut zzz = new_state.try_to_vec().unwrap();
+    
+    // msg!("zzz: {:?}", zzz.as_slice());
+
+    
+    // let  data: &mut[u8] = &mut[];
+    // *account.data.borrow_mut() =  new_state.try_to_vec().unwrap().as_mut_slice();
+    
+    
+
+    
     msg!("update-state finish");
     Ok(())
 }
 
-
 #[cfg(test)]
 mod test {
-
     use super::*;
 
     #[test]
@@ -49,5 +62,4 @@ mod test {
         let ix = InstructionData::try_from_slice(&[9, 3, 0, 0, 3, 0, 0, 0, 102, 111, 111]).unwrap();
         println!("z1: {:?}", ix);
     }
-
 }
--- tests/lib.rs
+++ tests/lib.rs
@@ -1,30 +1,34 @@
-use borsh::BorshSerialize;
+use borsh::{BorshSerialize};
 use solana_program::instruction::{AccountMeta, Instruction};
 use solana_program_test::*;
 use solana_sdk::{account::Account, pubkey::Pubkey, signature::Signer, transaction::Transaction};
-use update_state::{process_instruction, InstructionData};
+use update_state::{process_instruction, State, InstructionData};
 
 #[tokio::test]
 async fn test_program() {
-    println!("bpf test");
     let program_id = Pubkey::new_unique();
     let state_pubkey = Pubkey::new_unique();
 
     let mut program_test = ProgramTest::new("update_state", program_id, processor!(process_instruction));
-    program_test.add_account(state_pubkey, Account { lamports: 7777777777777777777, owner: program_id, ..Account::default() });
+    let data = State{counter: 0, f1: 0, f2: String::from("")}.try_to_vec().unwrap();
+    program_test.add_account(state_pubkey, Account { lamports: 77777, data, owner: program_id, ..Account::default() });
     
 
     let (mut banks_client, payer, recent_blockhash) = program_test.start().await;
 
-    let ix_data = InstructionData { f1: 777, f2: String::from("foo") }.try_to_vec().unwrap();
-    println!("ix_data: {:?}", ix_data);
+    
 
 
-    // Make tx
+    // Call program first time
+    let init_data = InstructionData { f1: 1, f2: String::from("foo") }.try_to_vec().unwrap();
     let mut transaction = Transaction::new_with_payer(
-        &[Instruction::new_with_bytes(program_id, &ix_data, vec![AccountMeta::new(state_pubkey, false)])],
+        &[Instruction::new_with_bytes(program_id, &init_data, vec![AccountMeta::new(state_pubkey, false)])],
         Some(&payer.pubkey()),
     );
     transaction.sign(&[&payer], recent_blockhash);
     banks_client.process_transaction(transaction).await.unwrap();
+
+    // // Call program
+    // let state_account = banks_client.get_account(state_pubkey).await;
+    // println!("state_account: {:?}", state_account);
 }
